c349e91cf9f414f41fc274396401cbf3a0d0ce33
David Chamont <chamont@in2p3.fr>
2018-02-22 13:06:27 +0100

    Docker used for the externals

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t gitlab-registry.in2p3.fr/chamont/fast-matrix .
-docker push gitlab-registry.in2p3.fr/chamont/fast-matrix
+docker build -t gitlab-registry.in2p3.fr/chamont/fast5x5 .
+docker push gitlab-registry.in2p3.fr/chamont/fast5x5
