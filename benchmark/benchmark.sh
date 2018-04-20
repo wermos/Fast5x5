a3039f635bc4e8ad13cb61ae0c161108e9bca6d7
Hadrien Grasland <grasland@lal.in2p3.fr>
2018-04-20 09:31:37 +0200

    Replace racey process substitution with a simpler stderr/stdout swap

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -14,21 +14,32 @@ for size in $(seq 3 8); do
     echo -e "\t Simple"
     g++ gemm_simple.cpp -DSIZE=$size -O3 -march=native
     for i in $(seq 1 ${REPEAT}); do
-        ${TIME} -f "%U, " ./a.out 2> >(tr -d '\n' | tee -a ${FILE}_simple.res) && echo
+        # GNU time produces its data on stderr, and we want to post-process it.
+        # But bash's pipe operator only works on stdout. So we need to swap
+        # stdout and stderr first.
+        /usr/bin/time -f "%U, " ./a.out 3>&1 1>&2 2>&3 3>&-                    \
+                    | tr -d '\n' | tee -a ${FILE}_simple.res                   \
+        && echo
     done
     echo >> ${FILE}_simple.res
 
     echo -e "\t Eigen"
     g++ gemm_eigen.cpp -DSIZE=$size -O3 -march=native
     for i in $(seq 1 ${REPEAT}); do
-        ${TIME} -f "%U, " ./a.out 2> >(tr -d '\n' | tee -a ${FILE}_eigen.res) && echo
+        # Same as above, only targeting a different file
+        /usr/bin/time -f "%U, " ./a.out 3>&1 1>&2 2>&3 3>&-                    \
+                    | tr -d '\n' | tee -a ${FILE}_eigen.res                    \
+        && echo
     done
     echo >> ${FILE}_eigen.res
 
     echo -e "\t Custom"
     g++ gemm_custom.cpp -DSIZE=$size -O3 -march=native
     for i in $(seq 1 ${REPEAT}); do
-        ${TIME} -f "%U, " ./a.out 2> >(tr -d '\n' | tee -a ${FILE}_custom.res) && echo
+        # Same as above, only targeting a different file
+        /usr/bin/time -f "%U, " ./a.out 3>&1 1>&2 2>&3 3>&-                    \
+                    | tr -d '\n' | tee -a ${FILE}_custom.res                   \
+        && echo
     done
     echo >> ${FILE}_custom.res
 
