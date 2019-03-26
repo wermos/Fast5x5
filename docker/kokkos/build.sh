8bf6b223552167ff84953a40c4030c45f1820130
David Chamont <chamont@in2p3.fr>
2019-03-26 14:16:11 +0100

    reorganisation with docker subdirectoories

---
+++ docker/kokkos/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t gitlab-registry.in2p3.fr/codeursintensifs/fast5x5/kokkos:gsoc2019 .
---
+++ docker/kokkos/push.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker push gitlab-registry.in2p3.fr/codeursintensifs/fast5x5/kokkos:gsoc2019
