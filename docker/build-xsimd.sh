bca70f08cffe49569833e4169667053db9b67f7f
David Chamont <chamont@in2p3.fr>
2019-03-26 11:52:34 +0100

    Try a new docker image

---
+++ docker/build-xsimd.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t gitlab-registry.in2p3.fr/codeursintensifs/fast5x5/xsimd:gsoc2019 .
---
+++ docker/push-xsimd.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker push gitlab-registry.in2p3.fr/codeursintensifs/fast5x5/xsimd:gsoc2019
