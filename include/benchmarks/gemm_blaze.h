7e1809bff25c730f43f8ca00be237bb2daeff731
wermos <63574588+wermos@users.noreply.github.com>
2022-06-14 23:02:22 +0530

    Wrote a blaze benchmark suite.

---
+++ include/benchmarks/gemm_blaze.h
@@ -0,0 +1,44 @@
+#ifndef GEMM_BLAZE_H
+#define GEMM_BLAZE_H
+
+#include "benchmark/benchmark.h"
+#include "blaze/Math.h"
+#include "gemm_header.h"
+
+static void gemm_blaze(benchmark::State& state) {
+    blaze::StaticMatrix<float, SIZE, SIZE> pa, pb, pc;
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            pa[i][j] = i + j;
+        }
+    }
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            float val;
+            if (i == 0 && j == 1)
+                val = -1;
+            else if (i == 1 && j == 0)
+                val = 1;
+            else if (i > 1 && i == j && i % 2)
+                val = -1;
+            else if (i > 1 && i == j && !(i % 2))
+                val = 1;
+            else
+                val = 0;
+            pb[i][j] = val;
+        }
+    }
+
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(pc);
+
+        pc = pa * pb;
+        pa = pb * pc;
+
+        // std::cout << pa << std::endl;
+    }
+}
+
+#endif  // GEMM_BLAZE_H
--- src/benchmarks/benchmark.cpp
+++ src/benchmarks/benchmark.cpp
@@ -1,20 +1,25 @@
 #include "benchmark/benchmark.h"
 
-#include "benchmarks/gemm_simple.h"
-#include "benchmarks/gemm_eigen.h"
+#include "benchmarks/gemm_blaze.h"
 #include "benchmarks/gemm_custom.h"
+#include "benchmarks/gemm_eigen.h"
+#include "benchmarks/gemm_simple.h"
 
 constexpr int repetitions = 20;
 
-#define BENCHMARK_SUITE(x) BENCHMARK(x) \
-	->Threads(1)->Threads(8)            \
-	->Repetitions(repetitions)          \
-    ->DisplayAggregatesOnly(true)       \
-    ->UseRealTime();
+#define BENCHMARK_SUITE(x)            \
+    BENCHMARK(x)                      \
+        ->Threads(1)                  \
+        ->Threads(8)                  \
+        ->Repetitions(repetitions)    \
+        ->DisplayAggregatesOnly(true) \
+        ->UseRealTime();
 
 BENCHMARK_SUITE(gemm_simple);
 
 BENCHMARK_SUITE(gemm_eigen);
+
+BENCHMARK_SUITE(gemm_blaze);
 
 BENCHMARK_SUITE(gemm_custom);
 
