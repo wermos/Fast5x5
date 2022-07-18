54e050b46f86493d36299d0179250b1a566f7502
wermos <63574588+wermos@users.noreply.github.com>
2022-07-19 01:05:31 +0530

    Revert "Changed all instances of `int`s to `std::size_t`."
    This reverts commit 36d8abbd97320c1790e919757755a85a97137068.

--- include/fast5x5/fast5x5.hpp
+++ include/fast5x5/fast5x5.hpp
@@ -2,14 +2,13 @@
 #define FAST5x5_HPP
 
 #include <array>
-#include <cstddef> // for the std::size_t data type
 #include <ostream>
 #include <utility>
 #include <xsimd/xsimd.hpp>
 
 namespace xs = xsimd;
 
-constexpr std::size_t nearest_power_of_two(std::size_t min_value, std::size_t current_value) {
+constexpr int nearest_power_of_two(int min_value, int current_value) {
     // Computes the nearest power of two relative to `min_value` starting from
     // the power of two `current_value`
     return min_value <= current_value
@@ -17,17 +16,17 @@ constexpr std::size_t nearest_power_of_two(std::size_t min_value, std::size_t cu
                : nearest_power_of_two(min_value, current_value * 2);
 }
 
-template <typename T, std::size_t NRows, std::size_t NCols>
+template <typename T, int NRows, int NCols>
 class BaseMatrix;
 
-template <typename T, std::size_t NCols>
+template <typename T, int NCols>
 class Vector;
 
 template <typename M>
 static inline void matrix_add(M &a, M &b, M &c) {
     using batch_t = typename M::batch_t;
-    for (std::size_t i = 0; i < M::NRows; i++) {
-        std::size_t index = M::VecSize * i;
+    for (int i = 0; i < M::NRows; i++) {
+        int index = M::VecSize * i;
         batch_t row_a(&a.array[index], xs::aligned_mode());
         batch_t row_b(&b.array[index], xs::aligned_mode());
         row_a += row_b;
@@ -38,8 +37,8 @@ static inline void matrix_add(M &a, M &b, M &c) {
 template <typename M>
 static inline void matrix_sub(M &a, M &b, M &c) {
     using batch_t = typename M::batch_t;
-    for (std::size_t i = 0; i < M::NRows; i++) {
-        std::size_t index = M::VecSize * i;
+    for (int i = 0; i < M::NRows; i++) {
+        int index = M::VecSize * i;
         batch_t row_a(&a.array[index], xs::aligned_mode());
         batch_t row_b(&b.array[index], xs::aligned_mode());
         row_a -= row_b;
@@ -47,7 +46,7 @@ static inline void matrix_sub(M &a, M &b, M &c) {
     }
 }
 
-template <typename T, std::size_t l, std::size_t m, std::size_t n>
+template <typename T, int l, int m, int n>
 static inline void matrix_mul_m_m(BaseMatrix<T, l, m> &a,
                                   BaseMatrix<T, m, n> &b,
                                   BaseMatrix<T, l, n> &c) {
@@ -58,20 +57,20 @@ static inline void matrix_mul_m_m(BaseMatrix<T, l, m> &a,
      * in a row of C in one vector. This use only vector operations so the
      * compiler can optimize easily.
      */
-    static constexpr std::size_t LeftVecSize = BaseMatrix<T, l, m>::VecSize;
-    static constexpr std::size_t RightVecSize = BaseMatrix<T, m, n>::VecSize;
+    static constexpr int LeftVecSize = BaseMatrix<T, l, m>::VecSize;
+    static constexpr int RightVecSize = BaseMatrix<T, m, n>::VecSize;
     using batch_t = xs::batch<T, RightVecSize>;
     /* Algorithm:
      * For each row of C matrix:
-     *  * Load one element of A and broadcast it std::size_to vector.
-     *  * Load the corresponding row of B std::size_to vector.
+     *  * Load one element of A and broadcast it into vector.
+     *  * Load the corresponding row of B into vector.
      *  * Computes element-wise product between these two vector.
      *  * Repeat for each element of A in the row and sum these results.
      *  * Store the summation as corresponding row of C.
      */
-    for (std::size_t i = 0; i < l; i++) {
+    for (int i = 0; i < l; i++) {
         batch_t res = xs::zero<batch_t>();
-        for (std::size_t j = 0; j < m; j++) {
+        for (int j = 0; j < m; j++) {
             batch_t factor(a.array[LeftVecSize * i + j]);
             batch_t row(&b.array[RightVecSize * j], xs::aligned_mode());
             res += factor * row;
@@ -80,7 +79,7 @@ static inline void matrix_mul_m_m(BaseMatrix<T, l, m> &a,
     }
 }
 
-template <typename T, std::size_t l, std::size_t m, std::size_t n>
+template <typename T, int l, int m, int n>
 static inline void matrix_mul_mt_m(BaseMatrix<T, m, l> &a,
                                    BaseMatrix<T, m, n> &b,
                                    BaseMatrix<T, l, n> &c) {
@@ -90,12 +89,12 @@ static inline void matrix_mul_mt_m(BaseMatrix<T, m, l> &a,
      * This code is nearly identical to matrix_mul_m_m but we
      * run through A in column instead of row
      */
-    static constexpr std::size_t LeftVecSize = BaseMatrix<T, m, l>::VecSize;
-    static constexpr std::size_t RightVecSize = BaseMatrix<T, m, n>::VecSize;
+    static constexpr int LeftVecSize = BaseMatrix<T, m, l>::VecSize;
+    static constexpr int RightVecSize = BaseMatrix<T, m, n>::VecSize;
     using batch_t = xs::batch<T, RightVecSize>;
-    for (std::size_t i = 0; i < l; i++) {
+    for (int i = 0; i < l; i++) {
         batch_t res = xs::zero<batch_t>();
-        for (std::size_t j = 0; j < m; j++) {
+        for (int j = 0; j < m; j++) {
             batch_t factor(a.array[LeftVecSize * j + i]);
             batch_t row(&b.array[RightVecSize * j], xs::aligned_mode());
             res += factor * row;
@@ -104,7 +103,7 @@ static inline void matrix_mul_mt_m(BaseMatrix<T, m, l> &a,
     }
 }
 
-template <typename T, std::size_t l, std::size_t m, std::size_t n>
+template <typename T, int l, int m, int n>
 static inline void matrix_mul_m_mt(BaseMatrix<T, l, m> &a,
                                    BaseMatrix<T, n, m> &b,
                                    BaseMatrix<T, l, n> &c) {
@@ -113,26 +112,26 @@ static inline void matrix_mul_m_mt(BaseMatrix<T, l, m> &a,
      * where A, B and C are BaseMatrix objects.
      * This is slower than A*B product due to mandatory reduction.
      * The idea is that we don't need to transpose B beforehand. Since we have
-     * to load columns of t(B) std::size_to vector register we can load row of B
+     * to load columns of t(B) into vector register we can load row of B
      * instead. Assuming that B is store in row order, row of B are correctly
      * aligned for loading.
      */
-    static constexpr std::size_t VecSize =
+    static constexpr int VecSize =
         BaseMatrix<T, l, m>::VecSize;  // VecSize are the same for left and
                                        // right matrices
-    static constexpr std::size_t ResVecSize = BaseMatrix<T, l, n>::VecSize;
+    static constexpr int ResVecSize = BaseMatrix<T, l, n>::VecSize;
     using batch_t = xs::batch<T, VecSize>;
     /* Algorithm:
      * For each C matrix element:
-     *  * Load corresponding row of A std::size_to vector.
-     *  * Load corresponding column of t(B) => Load corresponding row of B std::size_to
+     *  * Load corresponding row of A into vector.
+     *  * Load corresponding column of t(B) => Load corresponding row of B into
      * vector.
      *  * Computes dot product between the two vector.
      *  * Store the resulting scalar of C.
      */
-    for (std::size_t i = 0; i < l; i++) {
+    for (int i = 0; i < l; i++) {
         batch_t row(&a.array[VecSize * i]);
-        for (std::size_t j = 0; j < n; j++) {
+        for (int j = 0; j < n; j++) {
             batch_t row_transpose(&b.array[VecSize * j], xs::aligned_mode());
             // Dot is a slow operation but it will get better with newer
             // hardware.
@@ -141,21 +140,21 @@ static inline void matrix_mul_m_mt(BaseMatrix<T, l, m> &a,
     }
 }
 
-template <typename T, std::size_t l, std::size_t m>
+template <typename T, int l, int m>
 static inline void matrix_mul_m_v(BaseMatrix<T, l, m> &a, Vector<T, m> &b,
                                   Vector<T, l> &c) {
-    static constexpr std::size_t VecSize = BaseMatrix<T, l, m>::VecSize;
+    static constexpr int VecSize = BaseMatrix<T, l, m>::VecSize;
 
     using batch_t = xs::batch<T, VecSize>;
     batch_t vector(&b.array[0], xs::aligned_mode());
 
-    for (std::size_t i = 0; i < l; i++) {
+    for (int i = 0; i < l; i++) {
         batch_t row(&a.array[VecSize * i], xs::aligned_mode());
         c.array[i] = xs::hadd(row * vector);
     }
 }
 
-template <typename T, std::size_t Size>
+template <typename T, int Size>
 class Inverse {
     using M = BaseMatrix<T, Size, Size>;
 
@@ -170,10 +169,10 @@ class Inverse {
         using batch_t = typename M::batch_t;
 
         // Cholesky decomposition
-        for (std::size_t i = 0; i < M::NRows; i++) {
+        for (int i = 0; i < M::NRows; i++) {
             batch_t sum_row = xs::zero<batch_t>();
 
-            for (std::size_t j = 0; j < i; j++) {
+            for (int j = 0; j < i; j++) {
                 batch_t cur_row(&r.array[j * M::VecSize], xs::aligned_mode());
                 batch_t factor(r.array[j * M::VecSize + i]);
                 sum_row += cur_row * factor;
@@ -189,10 +188,10 @@ class Inverse {
         M inv_r;
         static M Id(1);
         // Inversion of R by backward substitution
-        for (std::size_t i = (M::NRows - 1); i >= 0; i--) {
+        for (int i = (M::NRows - 1); i >= 0; i--) {
             batch_t sum_row = xs::zero<batch_t>();
 
-            for (std::size_t j = (M::NRows - 1); j > i; j--) {
+            for (int j = (M::NRows - 1); j > i; j--) {
                 batch_t cur_row(&inv_r.array[j * M::VecSize],
                                 xs::aligned_mode());
                 batch_t factor(r.array[i * M::VecSize + j]);
@@ -228,14 +227,14 @@ class Inverse<T, 2> {
     }
 };
 
-template <typename T, std::size_t NumberRows, std::size_t NumberCols>
+template <typename T, int NumberRows, int NumberCols>
 class BaseMatrix {
     /* Class for matrix object
      * Matrix object are linearized 2D array with padded line to fit vector size
      */
    protected:
     // We use the smallest vector size possible
-    static constexpr std::size_t VecSize = nearest_power_of_two(NumberCols, 1);
+    static constexpr int VecSize = nearest_power_of_two(NumberCols, 1);
 
     static xs::batch_bool<T, VecSize> select_mask() {
         return select_mask_impl(std::make_index_sequence<VecSize>());
@@ -252,12 +251,12 @@ class BaseMatrix {
 
    public:
     using ElementType = T;
-    static constexpr std::size_t NRows = NumberRows;
-    static constexpr std::size_t NCols = NumberCols;
+    static constexpr int NRows = NumberRows;
+    static constexpr int NCols = NumberCols;
 
     BaseMatrix() = default;
     BaseMatrix(T const a[]) {
-        for (std::size_t i = 0; i < NRows; i++) {
+        for (int i = 0; i < NRows; i++) {
             // In this case we copy data row by row
             batch_t row(&a[i * NCols], xs::unaligned_mode());
             if (NCols != VecSize)
@@ -267,13 +266,13 @@ class BaseMatrix {
     }
 
     BaseMatrix(T const a) {
-        for (std::size_t i = 0; i < NRows; i++) {
+        for (int i = 0; i < NRows; i++) {
             array[VecSize * i + i] = a;
         }
     }
 
     matrix_t operator=(T const a[]) {
-        for (std::size_t i = 0; i < NRows; i++) {
+        for (int i = 0; i < NRows; i++) {
             // In this case we copy data row by row
             batch_t row(&a[i * NCols], xs::unaligned_mode());
             if (NCols != VecSize)
@@ -295,10 +294,10 @@ class BaseMatrix {
             std::memcpy(&returned_array[0], this->array,
                         sizeof(T) * NRows * VecSize);
         } else {
-            for (std::size_t i = 0; i < NRows; i++) {
+            for (int i = 0; i < NRows; i++) {
                 // std::memcpy(&returned_array[i*NCols],
                 // &this->array[i*VecSize], sizeof(T)*NCols);
-                for (std::size_t j = 0; j < NCols; j++) {
+                for (int j = 0; j < NCols; j++) {
                     returned_array[i * NCols + j] =
                         this->array[i * VecSize + j];
                 }
@@ -309,14 +308,14 @@ class BaseMatrix {
 
     void store(T addr[]) const {
         if (NCols != VecSize) {
-            for (std::size_t i = 0; i < NRows - 1; i++) {
+            for (int i = 0; i < NRows - 1; i++) {
                 batch_t row(&this->array[i * VecSize], xs::aligned_mode());
                 row.store_unaligned(&addr[i * NCols]);
             }
             std::memcpy(&addr[(NRows - 1) * NCols],
                         &this->array[(NRows - 1) * VecSize], sizeof(T) * NCols);
         } else {
-            for (std::size_t i = 0; i < NRows; i++) {
+            for (int i = 0; i < NRows; i++) {
                 batch_t row(&this->array[i * VecSize], xs::aligned_mode());
                 row.store_unaligned(&addr[i * NCols]);
             }
@@ -325,8 +324,8 @@ class BaseMatrix {
 
     bool is_padding_zero() const {
         bool padding_ok = true;
-        for (std::size_t i = 0; i < NRows; i++) {
-            for (std::size_t j = NCols; j < VecSize; j++) {
+        for (int i = 0; i < NRows; i++) {
+            for (int j = NCols; j < VecSize; j++) {
                 if (array[i * VecSize + j] != 0)
                     padding_ok = false;
             }
@@ -338,19 +337,19 @@ class BaseMatrix {
     friend void matrix_add<matrix_t>(matrix_t &a, matrix_t &b, matrix_t &c);
     friend void matrix_sub<matrix_t>(matrix_t &a, matrix_t &b, matrix_t &c);
 
-    template <typename U, std::size_t l, std::size_t m, std::size_t n>
+    template <typename U, int l, int m, int n>
     friend void matrix_mul_m_m(BaseMatrix<U, l, m> &a, BaseMatrix<U, m, n> &b,
                                BaseMatrix<U, l, n> &c);
 
-    template <typename U, std::size_t l, std::size_t m, std::size_t n>
+    template <typename U, int l, int m, int n>
     friend void matrix_mul_mt_m(BaseMatrix<U, m, l> &a, BaseMatrix<U, m, n> &b,
                                 BaseMatrix<U, l, n> &c);
 
-    template <typename U, std::size_t l, std::size_t m, std::size_t n>
+    template <typename U, int l, int m, int n>
     friend void matrix_mul_m_mt(BaseMatrix<U, l, m> &a, BaseMatrix<U, n, m> &b,
                                 BaseMatrix<U, l, n> &c);
 
-    template <typename U, std::size_t l, std::size_t m>
+    template <typename U, int l, int m>
     friend void matrix_mul_m_v(BaseMatrix<U, l, m> &a, Vector<U, m> &b,
                                Vector<U, l> &c);
 
@@ -358,8 +357,8 @@ class BaseMatrix {
 
     friend std::ostream &operator<<(std::ostream &os, const matrix_t &mat) {
         // Matrix printing function
-        for (std::size_t i = 0; i < NRows; i++) {
-            for (std::size_t j = 0; j < NCols; j++) {
+        for (int i = 0; i < NRows; i++) {
+            for (int j = 0; j < NCols; j++) {
                 os << mat.array[i * BaseMatrix<T, NRows, NCols>::VecSize + j]
                    << ", ";
             }
@@ -372,7 +371,7 @@ class BaseMatrix {
     alignas(sizeof(T) * VecSize) T array[NRows * VecSize] = {0};
 };
 
-template <typename T, std::size_t NumberCols>
+template <typename T, int NumberCols>
 class Vector : public BaseMatrix<T, 1, NumberCols> {
    public:
     using BaseMatrix<T, 1, NumberCols>::BaseMatrix;
@@ -382,7 +381,7 @@ class Vector : public BaseMatrix<T, 1, NumberCols> {
     using vector_t = Vector<T, NumberCols>;
 
    private:
-    template <typename U, std::size_t l, std::size_t m>
+    template <typename U, int l, int m>
     friend void matrix_mul_m_v(BaseMatrix<U, l, m> &a, Vector<U, m> &b,
                                Vector<U, l> &c);
 
