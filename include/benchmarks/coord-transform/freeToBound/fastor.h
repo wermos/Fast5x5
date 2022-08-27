bf148a90103ee36e21a81dc6fd1cc873b9861004
wermos <63574588+wermos@users.noreply.github.com>
2022-08-27 23:40:18 +0530

    Wrote the coordinate transform benchmarks for Fastor.

---
+++ include/benchmarks/coord-transform/freeToBound/fastor.h
@@ -0,0 +1,20 @@
+#ifndef FREE_TO_BOUND_FASTOR_H 
+#define FREE_TO_BOUND_FASTOR_H
+
+#include "benchmark/benchmark.h"
+#include "fast5x5/fast5x5.hpp"
+#include "benchmarks/shared/common.hpp"
+
+static void freeToBound_fastor(benchmark::State& state) {
+    Fastor::Tensor<float, 6, 6> m1 = genRandomFastorMat<float, 6, 6>();
+    Fastor::Tensor<float, 6, 8> m2 = genRandomFastorMat<float, 6, 8>();
+    Fastor::Tensor<float, 6, 8> res;
+
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(res);
+
+		res = Fastor::matmul(m1, m2);
+    }
+}
+
+#endif // FREE_TO_BOUND_FASTOR_H
--- include/benchmarks/mat_coord_transform.hpp
+++ include/benchmarks/mat_coord_transform.hpp
@@ -3,13 +3,15 @@
 
 #include "benchmark/benchmark.h"
 
-#include "benchmarks/coord-transform/boundToFree/blaze.h"
-#include "benchmarks/coord-transform/boundToFree/custom.h"
-#include "benchmarks/coord-transform/boundToFree/eigen.h"
+#include "coord-transform/boundToFree/blaze.h"
+#include "coord-transform/boundToFree/custom.h"
+#include "coord-transform/boundToFree/eigen.h"
+#include "coord-transform/boundToFree/fastor.h"
 
-#include "benchmarks/coord-transform/freeToBound/blaze.h"
-#include "benchmarks/coord-transform/freeToBound/custom.h"
-#include "benchmarks/coord-transform/freeToBound/eigen.h"
+#include "coord-transform/freeToBound/blaze.h"
+#include "coord-transform/freeToBound/custom.h"
+#include "coord-transform/freeToBound/eigen.h"
+#include "coord-transform/freeToBound/fastor.h"
 
 #define BENCHMARK_SUITE(x)            \
     BENCHMARK(x)                      \
@@ -23,12 +25,16 @@ BENCHMARK_SUITE(boundToFree_eigen);
 
 BENCHMARK_SUITE(boundToFree_blaze);
 
+BENCHMARK_SUITE(boundToFree_fastor);
+
 BENCHMARK_SUITE(boundToFree_custom);
 
 /* free to bound */
 BENCHMARK_SUITE(freeToBound_eigen);
 
 BENCHMARK_SUITE(freeToBound_blaze);
+
+BENCHMARK_SUITE(freeToBound_fastor);
 
 BENCHMARK_SUITE(freeToBound_custom);
 
